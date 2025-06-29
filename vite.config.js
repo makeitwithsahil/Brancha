db1e42cabd9353e992da441e04c358735d59a81e
tenacitylogic@gmail.com <tenacitylogic@gmail.com>
2025-06-29 11:05:47 +0530

    Removed laoding animation

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  base: './', // ✅ This is crucial for Instagram and other WebViews
   plugins: [
     react(),
     VitePWA({
