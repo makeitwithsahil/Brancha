8a7d14d085a1af9256f77b5ef767533778c1e67b
tenacitylogic@gmail.com <tenacitylogic@gmail.com>
2025-12-26 20:41:12 +0530

    Added scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
@@ -12,6 +13,7 @@ export default function App() {
   return (
     <Router>
       <div className="min-h-screen bg-white">
+         <ScrollToTop />
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
