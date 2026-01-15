c9b85b53191ba7b4edb866a8fa5ccdaf017bd126
tenacitylogic@gmail.com <tenacitylogic@gmail.com>
2026-01-15 23:02:00 +0530

    import fix

--- scripts/generateBlogPost.js
+++ scripts/generateBlogPost.js
@@ -1,6 +1,10 @@
 // scripts/generateBlogPost.js
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // All 30 blog templates
 const templates = {
